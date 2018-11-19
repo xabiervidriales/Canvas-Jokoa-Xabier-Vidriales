2c3db2a5d360ce4e8cdfd9fb297f09d21fc9b59a
xabiervidriales <xabier.vidriales@alumni.mondragon.edu>
2018-11-19 18:18:14 +0100

    Initial commit

---
+++ canvas.js
@@ -0,0 +1,183 @@
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
+var ballRadius = 10;
+var x = canvas.width/2;
+var y = canvas.height-30;
+
+var dx = 2;
+var dy = -2;
+
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+
+var rightPressed = false;
+var leftPressed = false;
+
+var brickRowCount = 5;
+var brickColumnCount = 10;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 20;
+var brickOffsetTop = 20;
+var brickOffsetLeft = 30;
+
+var score = 0;
+
+var lives = 3;
+
+
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+function collisionDetection() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+            var b = bricks[c][r];
+			if(b.status == 1) {
+				if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+					dy = -dy;
+					b.status = 0;
+					score = score + 10;
+					if(score == (brickRowCount*brickColumnCount)*10) {
+						alert("ZORIONAK!!! LORTU DITUZUN PUNTUAK: "+score*lives);
+						document.location.reload();
+					}
+				}
+			}
+        }
+    }
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for(c=0; c<brickColumnCount; c++) {
+        for(r=0; r<brickRowCount; r++) {
+			if(bricks[c][r].status ==1) {
+				var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+				var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+				bricks[c][r].x = brickX;
+				bricks[c][r].y = brickY;
+				ctx.beginPath();
+				ctx.rect(brickX, brickY, brickWidth, brickHeight);
+				ctx.fillStyle = "#0095DD";
+				ctx.fill();
+				ctx.closePath();
+			}
+        }
+    }
+}
+
+function drawScore() {
+    ctx.font = "24px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Puntuazioa: "+score, 5, canvas.height-5);
+}
+
+function drawLives() {
+    ctx.font = "24px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Bizitzak: "+lives, canvas.width-120, canvas.height-5);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBricks();
+    drawBall();
+    drawPaddle();
+	drawScore();
+	drawLives();
+	collisionDetection();
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy*1.1;
+		}
+		else {
+			lives--;
+			if(!lives) {
+				alert("GALDU DUZU. SAIA ZAITEZ BERRIRO");
+				document.location.reload();
+			}
+			else {
+				x = canvas.width/2;
+				y = canvas.height-30;
+				dx = 2;
+				dy = -2;
+				paddleX = (canvas.width-paddleWidth)/2;
+			}
+		}
+	}
+	
+	if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 7;
+	}
+	else if(leftPressed && paddleX > 0) {
+    paddleX -= 7;
+}
+	
+    x += dx*1.25;
+    y += dy*1.25;
+	requestAnimationFrame(draw);
+}
+
+myAudio = new Audio('faded.WAV'); 
+if (typeof myAudio.loop == 'boolean')
+{
+    myAudio.loop = true;
+}
+else
+{
+    myAudio.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+	}, false);
+}
+
+draw();
